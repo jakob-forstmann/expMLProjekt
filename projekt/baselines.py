8c0ce38567ccd1855c19140c6eda6532778a1aed
jakob-forstmann <jakobforstmann@web.de>
2024-01-03 19:18:59 +0100

    create a simple CLI to select different algorithms

---
+++ projekt/baselines.py
@@ -0,0 +1,40 @@
+from sklearn.dummy import DummyRegressor
+from dataset_statistics import calculate_most_frequent_value,count_unique_valence_values
+from preprocessing import create_dataset
+from numpy import random
+from abc import abstractmethod
+
+class Estimator(type):
+    possible_estimators = {}
+    def __new__(cls,name,base,namespaces):
+        specific_estimator = super().__new__(cls,name,base,namespaces)
+        class_name = name.replace("_"," ")
+        cls.possible_estimators[class_name] = specific_estimator
+        return specific_estimator
+
+    def fit(self,x_train,x_test,y_train,y_test):
+        return self.model.fit(x_train,x_test)
+
+    @abstractmethod
+    def __str__(self):
+        pass
+    
+
+class Majority_Baseline(metaclass=Estimator):
+    def __init__(self):
+        spotify_songs = create_dataset()
+        self.frequent_value = calculate_most_frequent_value(spotify_songs)
+        self.model = DummyRegressor(strategy="constant",constant=self.frequent_value)
+
+    def __str__(self):
+        return f"Majority Baseline with most frequent valence{self.frequent_value}"
+
+
+class  Mean_Baseline(metaclass=Estimator):
+    def __init__(self):
+        self.model = DummyRegressor(strategy="mean")
+
+    def __str__(self):
+        return "Mean Baseline"
+
+
--- projekt/preprocessing.py
+++ projekt/preprocessing.py
@@ -1,3 +1,4 @@
+from functools import cache
 import pandas as pd 
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -6,6 +7,7 @@ import numpy as np
 def read_entire_dataset(file_name:str)-> pd.DataFrame:
     # the columns accoustiness,liveness and valence contain european 
     # decimals(,) but only the column valence is revelant here
+    print("read entire dataset")
     converter = {"valence":lambda x:np.float64(x.replace(",","."))}
     return pd.read_csv(file_name,converters=converter)
 
@@ -15,7 +17,7 @@ def keep_necessary_columns(songs:pd.DataFrame,columns_to_keep:[str]):
 def split_data(songs:pd.DataFrame):
     album_names = songs["track_album_name"].values
     songs["track_album_name"] = vectorize_album_name(album_names)
-    X = songs.drop(columns="valence")
+    X = songs.loc[:,songs.columns!="valence"]
     labels = songs["valence"]
     return train_test_split(X,labels,test_size=0.25,random_state=42)
 
@@ -24,13 +26,17 @@ def save_splitted_data(splitted_datasets:[np.array],file_names:[str]):
         dataset = pd.DataFrame(dataset_name)
         dataset.to_csv(file_name)
 
+def remove_strings_from_numeric_columns(songs:pd.DataFrame):
+    columns_to_clean = songs.loc[:,songs.columns!="track_album_name"]
+    cleaned = columns_to_clean.apply(lambda val:pd.to_numeric(val,errors="coerce")).dropna()
+    cleaned.insert(0,"track_album_name",songs["track_album_name"])
+    return cleaned
+
 def rescale_data_range(songs:pd.DataFrame,columns_to_scale:[np.array]):
     """ converts a value between 0-100 to a double between 0 and 1 
     for the passed columns"""
-    cleaned = songs["danceability"].apply(lambda val:pd.to_numeric(val,errors="coerce"))
-    songs = songs.drop(columns=["danceability"])
-    songs.insert(0,"danceability",cleaned)
-    songs.dropna(inplace=True)
+    songs = songs.dropna()
+    songs = remove_strings_from_numeric_columns(songs)
     songs.where(songs[columns_to_scale]<1,songs[columns_to_scale]/1000,inplace=True)
     return songs
 
@@ -43,6 +49,7 @@ def vectorize_album_name(album_names:list[str]):
     document_term_matrix =  vectorizer.fit_transform(album_names)
     return list(document_term_matrix.toarray())
 
+@cache
 def create_dataset():
     columns_to_use = ["danceability","track_album_name","tempo","loudness","mode","valence"]
     spotify_songs_complete  = read_entire_dataset("data/spotify_songs.csv")
