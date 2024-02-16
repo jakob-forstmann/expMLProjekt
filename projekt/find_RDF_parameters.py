e41b6aef22aabc68d6e21d1bcf8cd2e96d24703c
jakob-forstmann <jakobforstmann@web.de>
2024-02-16 15:14:30 +0100

    add new plots for linear regression and RDF

--- projekt/find_RDF_parameters.py
+++ projekt/find_RDF_parameters.py
@@ -1,8 +1,8 @@
 import pandas as pd 
 from sklearn.ensemble import RandomForestRegressor
 from preprocessing import build_model
-from evaluation import perform_grid_search_cv
-from dataset_statistics import plot_error_scores
+from evaluation import perform_grid_search_cv,evaluate_experiments
+from dataset_statistics import plot_results
 
 
 MAX_DEPTH_UPPER = 30
@@ -22,36 +22,33 @@ def get_rdf_for_experiments():
    return  RandomForestRegressor(random_state=0)
 
 
-def plot_results_rdf():
-    scores = evaluation_parameter["evaluation_metrics"]
-    columns_to_use = [dataset+"_"+metric for dataset in ["mean_train","mean_test"] for metric in scores]   
-    cv_results = pd.read_csv("evaluation_results/rdf_parameter_max_depth_30.csv")  
-    columns_to_use.append("param_model__max_depth")
-    MSE_results = cv_results[columns_to_use]
-    ten_estimators = MSE_results[::3]
-    twenty_estimators = MSE_results[1::3]
-    thirty_estimators = MSE_results[2::3]
-    train_results_ten =  ten_estimators[columns_to_use[0:2]]
-    test_results_ten  = ten_estimators[columns_to_use[2:4]]
-    train_results_twenty=  twenty_estimators[columns_to_use[0:2]]
-    test_results_twenty  = twenty_estimators[columns_to_use[2:4]]
-    train_results_thirty=  thirty_estimators[columns_to_use[0:2]]
-    test_results_thirty  = thirty_estimators[columns_to_use[2:4]]
-    plot_error_scores(train_results_ten,MSE_results["param_model__max_depth"][::3],
-    "Max_depth on the training dataset with 10 estimators ","max_depth","mean across 5 folds")
-    plot_error_scores(test_results_ten,MSE_results["param_model__max_depth"][::3],
-    "Max_depth on the evaluation dataset with 10 estimators ","max_depth","mean MSE across 5 folds")
-    plot_error_scores(train_results_twenty,MSE_results["param_model__max_depth"][1::3],
-    "Max_depth on the training dataset with 20 estimators","max_depth","mean MSE across 5 folds")
-    plot_error_scores(test_results_twenty,MSE_results["param_model__max_depth"][1::3],
-    "Max_depth on the evaluation dataset with 20 estimators ","max_depth","mean MSE across 5 folds")
-    plot_error_scores(train_results_thirty,MSE_results["param_model__max_depth"][2::3],
-    "Max_depth on the training dataset with 30 estimators","max_depth","mean MSE across 5 folds")
-    plot_error_scores(test_results_thirty,MSE_results["param_model__max_depth"][2::3],
-    "Max_depth on the evaluation dataset with 30 estimators","max_depth","mean MSE across 5 folds")
+def get_optimized_rdf():
+   return  RandomForestRegressor(random_state=0,n_estimators=30)
 
+def plot_results_rdf():
+    baselines = {"RMSE":-0.1969,"MEA":-0.1511}
+   
+def evaluate_default_rdf():
+    default_rdf = get_rdf_for_experiments()
+    default_rdf_30_estim = RandomForestRegressor(random_state=0)
+    piped_rdf = build_model(default_rdf)
+    piped_rdf_30_estim= build_model(default_rdf_30_estim)
+    RMSE_result_30_estim,MEA_result_30_estim = evaluate_experiments(piped_rdf_30_estim)
+    return RMSE_result_30_estim,MEA_result_30_estim
+
+
+def select_results(cv_results):
+    ten_estimators = cv_results[::3]
+    twenty_estimators = cv_results[1::3]
+    thirty_estimators = cv_results[2::3]
+    results_description = ["Ten Estimators","Twenty Estimators","30 Estimators"]
+    return [ten_estimators,twenty_estimators,thirty_estimators],results_description
 
 
 if __name__ =="__main__":
+    # might take a long time 
     #find_rdf_parameters()
-    plot_results_rdf()
+    RMSE_results_default,MEA_results_default = evaluate_default_rdf()
+    baseline = {"default RDF RMSE":RMSE_results_default,"default RDF MEA":MEA_results_default}
+    max_depth_range = list(range(1,MAX_DEPTH_UPPER))
+    plot_results("evaluation_results/rdf_parameter.csv",max_depth_range,select_results,baseline)
--- projekt/find_lasso_parameters.py
+++ projekt/find_lasso_parameters.py
@@ -3,9 +3,10 @@ from sklearn.linear_model import Lasso
 from sklearn.linear_model import LinearRegression
 from preprocessing import build_model
 from find_DT_parameters import evaluate_experiments,perform_grid_search_cv
+from dataset_statistics import plot_results
+
 
 alpha_range = list(range(1,40,2))
-alpha_range.append(0.1)
 file_name = "evaluation_results/lasso_evaluation.csv"
 
 evaluation_parameter = {
@@ -16,6 +17,10 @@ evaluation_parameter = {
 def get_lasso_model_for_experiments():
     return Lasso(random_state=0)
 
+
+def get_optimized_linear_model():
+    return LinearRegression()
+
 def find_lasso_parameters():
     """ evaluate a lasso model,e.g. a linear regression 
     with added regulaziation with optional intercept 
@@ -24,35 +29,34 @@ def find_lasso_parameters():
     perform_grid_search_cv(evaluation_parameter,piped_model,file_name)
    
 
-def find_OLS_parameter():
+def train_OLS():
     """ trains a ordinary Linear Regression(OLS) model
     with and without a bias.The OLS is used to train a model
     without regulazation since setting alpha to zero for the 
     lasso is discouraged in the documentation"""
     piped_liner = build_model(LinearRegression())
     piped_linear_unbiased = build_model(LinearRegression(fit_intercept=False))
-    _,test_RMSE_with_bias,_,test_MEA_with_bias = evaluate_experiments(piped_liner)
-    _,test_RMSE_unbiased,_,test_MEA_unbiased = evaluate_experiments(piped_linear_unbiased)
+    test_RMSE_with_bias,test_MEA_with_bias = evaluate_experiments(piped_liner)
+    test_RMSE_unbiased,test_MEA_unbiased = evaluate_experiments(piped_linear_unbiased)
+    print("TEST RMSE WITH BIAS",round(test_RMSE_with_bias,4))
+    print("test MEA  WITH BIAS",round(test_MEA_with_bias,4))
+    print("TEST RMSE WITHOUT BIAS",round(test_RMSE_unbiased,4))
+    print("test MEA  WITHOUT BIAS",round(test_MEA_unbiased,4))
     return test_RMSE_unbiased,test_RMSE_with_bias,test_MEA_with_bias,test_MEA_unbiased
     
 
-def plot_linear_regression_results():
-    lasso_results = pd.read_csv(file_name)
-    lasso_results_unbiased = lasso_results[0::2]
-    print(lasso_results_unbiased)
-    print("="*42)
-    lasso_results_with_bias = lasso_results[1::2]
-    columns_to_use = ["mean_test_neg_root_mean_squared_error","mean_test_neg_mean_absolute_error"]
-    RMSE_test_unbiased  = lasso_results_unbiased[columns_to_use]
-    RMSE_test_with_bias = lasso_results_with_bias[columns_to_use]
-    print(f"ubiased: {RMSE_test_unbiased} \n biased {RMSE_test_with_bias}")
-    test_RMSE_unbiased,test_RMSE_with_bias,test_MEA_with_bias,test_MEA_unbiased = find_OLS_parameter()
-    print(test_MEA_with_bias)
-    print("=")
-    print(test_MEA_unbiased)
-    print("RMSE")
-    print(test_RMSE_unbiased)
-    print(test_RMSE_with_bias)
-
-find_lasso_parameters()
-plot_linear_regression_results()
+def select_linear_regression_results(cv_results):
+    lasso_results_unbiased = cv_results[0::2]
+    lasso_results_with_bias = cv_results[1::2]
+    results_description = ["Model without Intercept","Model with Interept"]
+    parameter_description="alpha range"
+    return [lasso_results_unbiased,lasso_results_with_bias],results_description,parameter_description
+   
+    
+if __name__ =="__main__":
+    #find_lasso_parameters()
+    test_RMSE_unbiased,test_RMSE_with_bias,test_MEA_with_bias,test_MEA_unbiased = train_OLS()        
+    unbiased_baselines =  {"RMSE score for OLS":test_RMSE_unbiased,"MEA score for OLS":test_MEA_unbiased}
+    baselines_with_bias =  {"RMSE score for OLS":test_RMSE_with_bias,"MEA score for OLS":test_MEA_with_bias}
+    plot_results(file_name,alpha_range,select_linear_regression_results,[unbiased_baselines,baselines_with_bias])
+    
